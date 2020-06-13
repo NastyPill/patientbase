77c37f946d82d5be156b4fcda1d219242264cbc5
NastyPill <DJPonnYash.Vinyl@gmail.com>
2020-06-13 15:11:23 +0700

    db template in liqubase

---
+++ src/test/java/ru/patientbase/mainAPI/MainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.patientbase.mainAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
